a59b481c7abdf168ea761ea9a60869c4d58c2605
Craig Harrison <craigrun@gmail.com>
2016-03-20 10:29:33 -0600

    command line arguments

--- flight.go
+++ flight.go
@@ -3,15 +3,51 @@ package main
 import (
 	"fmt"
 	"github.com/gnarlyskier/flight/metar"
+	"os"
+	"strings"
 )
 
+type cmd func([]string)
+
+var commands = map[string]cmd{
+	"metar": metarCmd,
+}
+
 func main() {
-	fmt.Println("Flight Calculator")
-	metars, err := metar.QueryStationRadius("KBDU", 40, 3, true)
+	if len(os.Args) < 2 {
+		printUsage()
+		return
+	}
+	command, exists := commands[os.Args[1]]
+	if !exists {
+		printUsage()
+		return
+	}
+	command(os.Args[2:])
+}
+
+func printUsage() {
+	fmt.Println("usage: flight <command> arg1 arg2...")
+	keys := []string{}
+	for k := range commands {
+		keys = append(keys, k)
+	}
+	fmt.Println("Available commands: " + strings.Join(keys, ", "))
+}
+
+func metarCmd(argv []string) {
+	if len(argv) < 1 {
+		fmt.Println("usage: flight metar STATION1 STATION2 ...")
+	}
+	metars, err := metar.QueryStations(argv, 24, true)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	printMetars(metars)
+}
+
+func printMetars(metars []metar.Metar) {
 	for _, m := range metars {
 		fmt.Println(m)
 	}
--- metar/query_stations.go
+++ metar/query_stations.go
@@ -1,11 +1,19 @@
 package metar
 
 import (
+	"errors"
 	"net/url"
 	"strings"
 )
 
 func QueryStations(stations []string, hoursBeforeNow float64, mostRecentOnly bool) ([]Metar, error) {
+	// stations should be exactly 4 characters
+	for _, s := range stations {
+		if len(s) != 4 {
+			return []Metar{}, errors.New("station ids must be exactly 4 characters: " + s)
+		}
+	}
+
 	parameters := url.Values{}
 	parameters.Add("stationString", strings.Join(stations, ","))
 
