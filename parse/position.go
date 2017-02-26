876ca7ce09ee27148ead92adc543fb5bf54aa6f1
Craig Harrison <craigrun@gmail.com>
2017-02-25 17:37:19 -0700

    position lat,lon support for relative modifiers

--- parse/position.go
+++ parse/position.go
@@ -11,15 +11,38 @@ import (
 
 // Parse a position string.
 // Several formats are accepted, e.g.,
-// -105.03,45.42
+// 45.42,-105.03
 // KBDU
 // BJC
 // KBDU+5N+3W
 // KBDU+8@340
+// KBDU+10W+8@340
+// 45.42,-105.03+5N+3W
 func ParsePos(natfix data.Natfix, pos string) (geo.Coord, error) {
 	if len(pos) == 0 {
 		return geo.ErrCoord(), errors.New("empty position string")
 	}
+
+	// Parse station and any additional modifiers
+	var position string
+	var modifiers []string
+	if split := strings.Split(pos, "+"); len(split) == 0 {
+		// Unreachable
+		panic("expected non-empty position string")
+	} else {
+		position = split[0]
+		modifiers = split[1:len(split)]
+	}
+
+	if c, err := parseStart(natfix, position); err != nil {
+		return geo.ErrCoord(), err
+	} else {
+		return parseAndApplyModifiers(c, modifiers)
+	}
+}
+
+// A string containing a Lat,Lon or station id
+func parseStart(natfix data.Natfix, pos string) (geo.Coord, error) {
 	if strings.ContainsRune(pos, ',') {
 		// Lon,Lat coordinate
 		if c, err := geo.ParseLatLon(pos); err != nil {
@@ -27,51 +50,44 @@ func ParsePos(natfix data.Natfix, pos string) (geo.Coord, error) {
 		} else {
 			return c, nil
 		}
-	}
-
-	// Parse station and any additional modifiers
-	var station string
-	var modifiers []string
-	if split := strings.Split(pos, "+"); len(split) == 0 {
-		// Unreachable
-		panic("expected non-empty position string")
-	} else {
-		station = split[0]
-		modifiers = split[1:len(split)]
-	}
-	// Fetch station position and apply position modifiers
-	if c, err := natfix.Coord(station); err != nil {
+	} else if c, err := natfix.Coord(pos); err != nil {
 		return geo.ErrCoord(), err
 	} else {
-		// Apply relative position modifiers
-		// e.g., +5N+3W  +8@340
-		for _, m := range modifiers {
-			if strings.ContainsAny(m, "NSEWnsew") {
-				// Relative positional vector
-				// e.g., KBDU+50W
-				if v, err := parsePosOffset(m); err != nil {
-					return geo.ErrCoord(), err
-				} else {
-					// Only one of v.X or v.Y will be non-zero so order doesn't matter
-					c = c.AddToLon(v.X).AddToLat(v.Y)
-				}
-			} else if strings.ContainsRune(m, '@') {
-				// Relative directional vector
-				// e.g., KBDU+4@340
-				// Makes flat-earth assumption, not valid over large distances
-				if v, err := parseDirOffset(m); err != nil {
-					return geo.ErrCoord(), err
-				} else {
-					c = c.AddToLon(v.X).AddToLat(v.Y)
-				}
-			} else {
-				return geo.ErrCoord(), errors.New("Invalid modifier vector: " + m)
-			}
-		}
+		// Station position
 		return c, nil
 	}
 }
 
+// Apply position relative modifiers
+// e.g., +5N +3W +23@340
+func parseAndApplyModifiers(c geo.Coord, modifiers []string) (geo.Coord, error) {
+	for _, m := range modifiers {
+		if strings.ContainsAny(m, "NSEWnsew") {
+			// Relative positional vector
+			// e.g., KBDU+50W
+			if v, err := parsePosOffset(m); err != nil {
+				return geo.ErrCoord(), err
+			} else {
+				// Only one of v.X or v.Y will be non-zero so order doesn't matter
+				c = c.AddToLon(v.X).AddToLat(v.Y)
+			}
+		} else if strings.ContainsRune(m, '@') {
+			// Relative directional vector
+			// e.g., KBDU+4@340
+			// Makes flat-earth assumption, not valid over large distances
+			if v, err := parseDirOffset(m); err != nil {
+				return geo.ErrCoord(), err
+			} else {
+				c = c.AddToLon(v.X).AddToLat(v.Y)
+			}
+		} else {
+			return geo.ErrCoord(), errors.New("Invalid modifier vector: " + m)
+		}
+	}
+	return c, nil
+}
+
+// e.g., 23N 3W etc
 func parseDirOffset(s string) (geo.Vect, error) {
 	var v, dir float64
 	_, err := fmt.Sscanf(s, "%f@%f", &v, &dir)
@@ -83,6 +99,7 @@ func parseDirOffset(s string) (geo.Vect, error) {
 	return geo.HeadingFromAngle(theta).Mult(v), nil
 }
 
+// e.g., 26@340
 func parsePosOffset(s string) (geo.Vect, error) {
 	var v float64
 	var dir rune
